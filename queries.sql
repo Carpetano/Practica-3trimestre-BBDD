5b127d16fc026a5b4ae9972845d78b6ec982a0b8
unknown <kevincarpetano@protonmail.me>
2024-04-25 16:27:02 +0200

    dado formato al archivo de queries

--- queries.sql
+++ queries.sql
@@ -1,1 +1,23 @@
 -- Queries de la BBDD
+
+-- 1. Crea 2 consultas monotabla que se ejecuten regularmente en la aplicación. 
+-- Por ejemplo, si al conectarnos la app nos muestra las ofertas de los productos “SELECT ofertas FROM productos;”
+
+-- 2. Igual que en el apartado anterior, crea 2 consultas multitablas que se ejecutarán de manera frecuente en la aplicación  (JOIN)
+
+-- 3. Crea 2 inserciones, 2 modificaciones y 2 eliminaciones frecuentes que se llevarán a cabo en la aplicación. 
+
+-- 4. Imaginaros que vienen dos personas nuevas a vuestra empresa, os ayudarán a mejorar aspectos de la aplicación 
+-- ¿Qué vistas le proporcionaríais teniendo en cuenta las funciones que van a desarrollar (define también qué función desarrollarán)? 
+-- crea mínimo 2 vistas.
+
+-- 5. Define qué usuarios crearías para que puedan acceder al SGBD y qué permisos tendrán.
+
+-- 6. ¿Has detectado alguna consulta que se realiza constantemente? ¿echas de menos algún índice? por ejemplo, 
+-- si se busca siempre información por ciudad sería recomendable crear un índice en el campo ciudad.
+
+-- 7. ¿Qué triggers pensáis que necesita la base de datos?, crea 2 trigger por tabla.
+
+-- 8. ¿Qué procedimientos almacenados podría necesitar la base de datos?, crea 2 procedimientos almacenados.
+
+
